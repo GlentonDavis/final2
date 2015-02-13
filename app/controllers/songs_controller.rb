aeb7f2645112336fefbccf318c63988d99c3d344
GlentonDavis <gdavis2016@kellogg.northwestern.edu>
2015-02-12 20:24:04 -0600

    I am having a huge mental block
    Will need help with this one, sadly...

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,10 +2,38 @@ class SongsController <ApplicationController
 
   def index
     @songs = Song.all
+    
   end
 
   def show
-    @song = Song.find_by(params["id"])
+    @song = Song.find_by(id: params["id"])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    song_params = params.require(:song).permit!
+    Song.create(song_params)
+    redirect_to song_path
+  end
+
+  def edit
+    @song = Song.find_by(id: params["id"])
+  end
+
+  def update
+    song_params = params.require(:song).permit!
++    @song = Song.find_by(id: params["id"])
++    @song.update(song_params)
++    redirect_to songs_path
+  end
+
+  def destroy
+    @song = Song.find_by(id: params["id"])
++    @song.destroy
++    redirect_to songs_path
   end
 end
 
